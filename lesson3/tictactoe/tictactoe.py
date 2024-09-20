b1ff4a972373e0f5726981718814a02dee493011
Jonathan <33232878+unlikelykoala@users.noreply.github.com>
2024-09-19 20:50:44 -0400

    TTT, pylint changes

--- lesson3/tictactoe/tictactoe.py
+++ lesson3/tictactoe/tictactoe.py
@@ -12,6 +12,8 @@ PC_MARKER = '◯'
 GAMES_TO_WIN = 2
 PC_NAME = 'Wilbur'
 MIDDLE_SQUARE = 5
+HARD = '3'
+MEDIUM = '2'
 WINNING_LINES = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9],    # horizontal
     [1, 4, 7], [2, 5, 8], [3, 6, 9],    # vertical
@@ -45,7 +47,6 @@ def intro():
     enter_to_continue_clear()
     cowsay.pig(f'To win the match, you have to get {GAMES_TO_WIN} wins.')
     enter_to_continue_clear()
-    
 
 def player_wants_instructions():
     cowsay.pig('Would you like to see the instructions?')
@@ -98,7 +99,7 @@ def welcome_player(name):
     enter_to_continue_clear()
 
 def get_difficulty():
-    cowsay.pig(f'Tell me, how easy should I take it on you?\n'
+    cowsay.pig('Tell me, how easy should I take it on you?\n'
                '1 is easy, 2 is medium, and 3 is hard.')
 
     difficulty = input('Enter 1, 2, or 3: ')
@@ -122,17 +123,15 @@ def get_current_player(player_name, pc_name):
                    "I can't hear you!")
         prompt("Press '1' to go first or '2' to go second:")
         response = input()
-        clear_without_enter()   
+        clear_without_enter()
     if response == '1':
         return player_name
-    else:
-        return pc_name
+    return pc_name
 
 def switch_current_player(current_player, player_name, pc_name):
     if current_player == player_name:
         return pc_name
-    else:
-        return player_name
+    return player_name
 
 def init_score_table(player_name, pc_name):
     return [[player_name, 0], [pc_name, 0]]
@@ -153,7 +152,7 @@ def check_yn_input(response):
         cowsay.pig('Could you say that again?\n'
                    "I couldn't make that out.")
         response = input("Enter 'Y/y' or 'N/n': ")
-    
+
     return response
 
 def response_is_y(response):
@@ -162,8 +161,9 @@ def response_is_y(response):
 def get_match_winner(board, player_name, pc_name):
     if board[0][1] == GAMES_TO_WIN:
         return player_name
-    elif board[1][1] == GAMES_TO_WIN:
+    if board[1][1] == GAMES_TO_WIN:
         return pc_name
+    return None
 
 def is_match_winner(winner):
     return bool(winner)
@@ -181,12 +181,12 @@ def player_wins_match(player_name):
     cowsay.pig('Farmer Zuckerman won\'t be happy with me...\n')
     enter_to_continue_clear()
 
-    
 def pc_wins_match(pc_name):
     cowsay.pig('Oink! Oink!\n'
                "That's two out of three! I win!")
     enter_to_continue_clear()
-    cowsay.pig(f"Looks like ol' {pc_name} avoids the frying pan\n for another day!")
+    cowsay.pig(f"Looks like ol' {pc_name} avoids the frying pan\n "
+               "for another day!")
     enter_to_continue_clear()
 
 def end_of_match(winner, player_name, pc_name):
@@ -200,7 +200,7 @@ def init_board():
 
 def display_board(board):
     os.system('clear')
-    
+
     prompt(f"You are {HUMAN_MARKER}. {PC_NAME} is {PC_MARKER}.")
     print()
     print('     |     |')
@@ -224,11 +224,11 @@ def empty_squares(board):
 def join_or(seq, delimiter=', ', conjunction='or'):
     if not seq:
         return ''
-    elif len(seq) == 1:
+    if len(seq) == 1:
         return str(seq[0])
-    elif len(seq) == 2:
+    if len(seq) == 2:
         return f'{seq[0]} {conjunction} {seq[1]}'
-    
+
     str_seq = [str(ele) for ele in seq]
     minus_last = delimiter.join(str_seq[:-1])
     full_str = minus_last + f'{delimiter}{conjunction} {str_seq[-1]}'
@@ -246,30 +246,30 @@ def player_chooses_square(board):
         valid_choices = [str(num) for num in empty_squares(board)]
         prompt(f'Choose a square ({join_or(valid_choices)})')
         square = input().strip()
-        
+
         if square in valid_choices:
             break
-        
+
         prompt('Sorry, that\'s not a valid choice.')
-    
+
     board[int(square)] = HUMAN_MARKER
 
 def computer_chooses_square(board, difficulty):
     if len(empty_squares(board)) == 0:
         return
     square = None
-    if difficulty == '2':
+    if difficulty == MEDIUM:
         square = return_winner_or_at_risk_square(board, PC_MARKER)
-    
+
     if not square:
         square = return_winner_or_at_risk_square(board, HUMAN_MARKER)
 
     if not square and board[5] == EMPTY_MARKER:
         square = MIDDLE_SQUARE
-    
+
     if not square:
         square = random.choice(empty_squares(board))
-    
+
     board[square] = PC_MARKER
 
 def return_winner_or_at_risk_square(board, marker):
@@ -280,7 +280,7 @@ def return_winner_or_at_risk_square(board, marker):
             for square in line:
                 if board[square] == EMPTY_MARKER:
                     return square
-                
+
     return None
 
 def detect_winner(board, player_name, pc_name):
@@ -290,11 +290,11 @@ def detect_winner(board, player_name, pc_name):
             and board[sq2] == HUMAN_MARKER
             and board[sq3] == HUMAN_MARKER):
             return player_name
-        elif (board[sq1] == PC_MARKER
+        if (board[sq1] == PC_MARKER
             and board[sq2] == PC_MARKER
             and board[sq3] == PC_MARKER):
             return pc_name
-        
+
     return None
 
 def someone_won(board, player_name, pc_name):
@@ -305,7 +305,7 @@ def board_full(board):
 
 def play_tictactoe():
     intro()
-    
+
     player_name = get_player_name()
     clear_without_enter()
     welcome_player(player_name)
@@ -323,7 +323,8 @@ def play_tictactoe():
         while True:
             choose_square(board, current_player, difficulty)
             display_board(board)
-            current_player = switch_current_player(current_player, player_name, PC_NAME)
+            current_player = switch_current_player(current_player,
+                                                   player_name, PC_NAME)
             time.sleep(0.3)
             if someone_won(board, player_name, PC_NAME) or board_full(board):
                 break
@@ -341,8 +342,10 @@ def play_tictactoe():
             display_score(score_table)
             enter_to_continue_clear()
 
-        if is_match_winner(get_match_winner(score_table, player_name, PC_NAME)):
-            match_winner = get_match_winner(score_table, player_name, PC_NAME)
+        if is_match_winner(get_match_winner(score_table,
+                                            player_name, PC_NAME)):
+            match_winner = get_match_winner(score_table,
+                                            player_name, PC_NAME)
             end_of_match(match_winner, player_name, PC_NAME)
             if play_again():
                 play_tictactoe()
