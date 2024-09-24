14d4eb08478fab396a15fb620f28058c6a477dbe
Jonathan <33232878+unlikelykoala@users.noreply.github.com>
2024-09-24 12:09:40 -0400

    PY110, Twenty-one

---
+++ lesson3/twentyone/pedac.md
@@ -0,0 +1,100 @@
+input:
+output:
+
+Goal: get as close to 21 as possible without going over (bust)
+
+Setup:
+- 2 players: dealer and player
+- both get dealt a hand of 2 cards
+- player can see their two cards, but only one of dealer's cards
+
+Card values:
+- 2-10 are their number value
+- Jack/Queen/King = 10
+- Ace = 1 or 11
+    - 11 if it doesn't make total go over 21, else 2
+    - edge case: getting multiple aces
+
+Questions:
+- can an ace change value from 11 to 1 as you draw more cards?
+
+Player turn:
+- choose to hit (get another card) or stay
+- turn is over when player either stays or busts
+- bust means the game is over and the dealer won
+
+Dealer's turn:
+- when player stays, dealer's turn
+- strict rule for deciding ot hit or stay: hit until total is at least 17
+- if dealer busts, player wins
+
+Compare cards:
+- after both player and dealer stay, they compare the total value of the cards to see who has the highest value
+
+Data structures:
+- list of tuples for deck of cards (suit, num/face)
+    - init using 2 tuples, one for suits and one for nums/faces
+
+Algorithm:
+1. Initialize the deck
+    - list of suits and list of nums/faces
+    - combine into list of tuples
+    - note that suit doesn't actually matter except for game visuals. only the number of each value is important
+    - how to handle 11
+    -[(2, 'clubs', 2), ('jack', 'hearts', 10)....] (face, suit, value)
+2. Initialize scoreboard
+3. Instructions
+3. Deal player cards
+4. Deal dealer cards
+5. Reveal 1 dealer card to player
+6. Player Turn
+    - Hit or stay
+    - if bust, player loses
+7. Dealer turn
+    - hit until at least 17, then stay
+    - if bust, player wins
+8. Compare hands (if no busts)
+    - highest number is winner
+9. Record scores
+10. Play again?
+
+Player turn algorithm:
+1. Show the player his hand:
+    A: Calculate the point total for non-ace cards
+    B: Calculate ace value based on overall total
+        - Multiple aces how to handle
+    C: Add ace value to get full total
+    D: If over 21, bust! Dealer wins
+    E: Display each card along with its face and number of points
+        - make a note about how the ace value could change
+    F: Display full point total
+2. Show the player one card from the dealer's hand
+3. Ask player if he wants to hit or stay
+    A: If hit, repeat step 1 with new hand
+    B: If stay, move to dealer's turn
+
+Dealer's turn algorithm:
+1. Calculate the point total for non-ace cards
+2. Calculate ace value based on overall total
+3. Add ace value to get full total
+    - If over 21, bust! Dealer wins
+4. If under 17 points, hit
+    - repeat steps 1-4
+5. If over 17, stay.
+    - Compare cards
+
+Compare cards algorithm:
+1. Display all cards and point totals for both players
+2. Decide winner based on who has highest score
+3. Update scoreboard
+4. Display scoreboard
+5. If less than necesary wins, next round
+6. If necessary wins reached, end of match.
+    - play again?
+
+Using player turn and dealer turn to decide winner and display results:
+1. 
+
+
+NOTE: I need to fix the ace_total algorith
+- if aces_only_total + no_aces_total is more than 21, then i need to reduce at least 1 ace
---
+++ lesson3/twentyone/twentyone.py
@@ -0,0 +1,466 @@
+import os
+import random
+import sys
+import time
+
+from tabulate import tabulate
+
+
+CARD_VALUE_RULES = ["- All number cards are worth their face values.\n\
+    - e.g., a 2 of clubs is worth 2 points.",
+    "- Jacks, Queens, and Kings are worth 10 points.",
+    "- Aces can be either 1 point or 11 points.\n\
+    - If a player already has 10 or fewer points, the Ace is 11 points.\n\
+    - If a player already 11 or more points, the Ace is 1 point."
+]
+RULES = [
+    "The Goal: get your cards' total value as close to 21 \
+as possible without going over.\n\
+    - You can have a score of 21 exactly.\n\
+    - If you go over 21, you bust (lose).",
+    '1. Both you and the dealer are dealt two cards.\n\
+    - You can look at both of your cards as well one of the dealer\'s.',
+    '2. Your turn: decide if you want to hit (receive another card) \
+or stay (end your turn).',
+    "3. Dealer's turn: the dealer also decides to hit or stay.\n\
+    - The dealer must hit until their score is at least 17.\n\
+    - If the dealer goes over 21, they bust, which means you win!",
+    "4. After both you and the dealer stay, then you compare hands.\n\
+    - The highest score wins as long as it's not over 21.",
+]
+ROUNDS_TO_WIN = 3
+MAX_SCORE = 21
+ACE_MAX = 11
+ACE_MIN = 1
+DEALER_HIT_THRESHOLD = 17
+DEALER = 'Dealer'
+SUITS = ['clubs', 'diamonds', 'hearts', 'spades',]
+NAMES_AND_VALUES = (('ace', (ACE_MIN, ACE_MAX)),(2, 2), (3, 3), (4, 4),
+                    (5, 5), (6, 6), (7, 7), (8, 8), (9, 9),
+                    ('jack', 10), ('queen', 10), ('king', 10))
+
+
+def clear_terminal():
+    os.system('clear')
+
+def press_enter_to_continue_clear():
+    prompt("(Press 'Enter' to continue)", ending='')
+    input()
+    clear_terminal()
+
+def press_enter_clear_this_line():
+    prompt("(Press 'Enter' to continue)", ending='')
+    input()
+    print("\033[F\033[K", end='')
+    flush_print()
+    pause()
+
+def pause(seconds=0.3):
+    time.sleep(seconds)
+
+def transition(seconds=0.3):
+    pause(seconds)
+    clear_terminal()
+
+def prompt(message, ending='\n'):
+    print('==> ' + message, end=ending)
+
+def flush_print():
+    sys.stdout.flush()
+
+def get_name():
+    clear_terminal()
+    prompt('Please enter your name: ', ending='')
+    name = input().strip()
+    while not name:
+        transition()
+        prompt('You forgot to enter your name!')
+        print()
+        prompt('Name: ', ending='')
+        name = input().strip()
+
+    return name
+
+def welcome_player(player_name):
+    transition()
+    prompt(f'Welcome to the game of Twenty-one, {player_name}!')
+    print()
+    press_enter_to_continue_clear()
+    transition()
+
+def return_valid_yn_response(message, response):
+    while response not in ['y', 'yes', 'n', 'no']:
+        transition()
+        prompt('Sorry, it looks like you made a mistake in your input.')
+        print()
+        prompt(message)
+        prompt("Press 'y' or 'n': ", ending='')
+        response = input().casefold().strip()
+
+    return response
+
+def prompt_player_wants_instructions():
+    prompt('Would you like to see the instructions?')
+    print()
+    prompt("Press 'y' or 'n': ", ending='')
+    response = return_valid_yn_response('Would you like to see \
+the instructions?', input().casefold().strip())
+
+    transition()
+
+    return bool(response == 'y')
+
+def instructions():
+    prompt('The rules are simple:\n')
+    for rule in RULES:
+        press_enter_clear_this_line()
+        print(rule)
+        print()
+
+    press_enter_to_continue_clear()
+    prompt('The card values are as follows:\n')
+    for rule in CARD_VALUE_RULES:
+        press_enter_clear_this_line()
+        print(rule)
+        print()
+
+    press_enter_to_continue_clear()
+    prompt("That's all the instructions! Do you need to see them again?")
+    prompt("Press 'y' or 'n': ", ending='')
+    response = return_valid_yn_response("Do you need to see the \
+instructions again?", input().casefold().strip())
+    if response == 'y':
+        transition()
+        instructions()
+
+    transition()
+
+def init_deck():
+    return [(name, value, suit) for suit in SUITS
+                 for name, value in NAMES_AND_VALUES]
+
+def shuffle(deck):
+    random.shuffle(deck)
+
+def init_score_table(player_name):
+    return [[player_name, 0], [DEALER, 0]]
+
+def update_score_table(table, winner):
+    if winner == DEALER:
+        table[1][1] += 1
+    else:
+        table[0][1] += 1
+
+def display_scoreboard(table):
+    print('SCOREBOARD')
+    print(tabulate(table))
+    print()
+
+def display_game_start(player_name):
+    prompt(f"You need to win {ROUNDS_TO_WIN} rounds to win the game.")
+    print()
+    press_enter_to_continue_clear()
+    transition()
+    prompt(f"Ready, {player_name}? Let's start!")
+    print()
+    press_enter_to_continue_clear()
+
+def deal_card_from_deck(deck):
+    card = deck.pop()
+
+    return card
+
+def deal_both_hands(deck):
+    player_hand = []
+    dealer_hand = []
+
+    for _ in range(2):
+        card = deal_card_from_deck(deck)
+        player_hand.append(card)
+        card = deal_card_from_deck(deck)
+        dealer_hand.append(card)
+
+    return player_hand, dealer_hand
+
+def player_turn(deck, player_hand, dealer_hand):
+    transition()
+
+    while True:
+        points = calculate_hand_total_points(player_hand)
+
+        display_dealer_hand(dealer_hand)
+        display_player_hand(player_hand)
+        display_player_points(points)
+
+        hit_or_stay = prompt_player_hit_or_stay(points)
+        if hit_or_stay in ['s', 'stay']:
+            return 'stay'
+
+        player_hits(deck, player_hand)
+
+        points = calculate_hand_total_points(player_hand)
+        if is_bust(points):
+            return 'bust'
+
+def dealer_turn(deck, dealer_hand):
+    dealer_thinking()
+
+    while True:
+        points = calculate_hand_total_points(dealer_hand)
+
+        if is_bust(points):
+            return 'bust'
+
+        if dealer_will_stay(points):
+            return 'stay'
+
+        dealer_hits(deck, dealer_hand)
+
+def one_round(deck, player_hand, dealer_hand, player_name, score_table):
+    stay_or_bust = player_turn(deck, player_hand, dealer_hand)
+    player_points, dealer_points = \
+        compare_hands_return_both_scores(player_hand, dealer_hand)
+
+    if stay_or_bust == 'stay':
+        stay_or_bust = dealer_turn(deck, dealer_hand)
+
+        if stay_or_bust == 'stay':
+            player_points, dealer_points = \
+                compare_hands_return_both_scores(player_hand, dealer_hand)
+            winner = return_winner_after_comparing(player_points,
+                                                   dealer_points, player_name)
+        else:
+            winner = player_name
+
+    else:
+        winner = DEALER
+
+    display_round_results(winner, stay_or_bust, player_points, dealer_points)
+    if winner in [DEALER, player_name]:
+        update_score_table(score_table, winner)
+
+def calculate_ace_points(hand, no_ace_total):
+    hand_faces = [card[0] for card in hand]
+    aces_count = hand_faces.count('ace')
+    aces_only_total = aces_count * ACE_MAX
+    total_val = no_ace_total + aces_only_total
+
+    while aces_count and total_val > 21:
+        aces_only_total -= 10
+        total_val -= 10
+        aces_count -= 1
+
+    return aces_only_total
+
+def calculate_hand_total_points(hand):
+    no_ace_total = 0
+    for card in hand:
+        if card[0] != 'ace':
+            no_ace_total += card[1]
+
+    return no_ace_total + calculate_ace_points(hand, no_ace_total)
+
+def display_player_hand(player_hand):
+    prompt("You have: ", ending='')
+    if len(player_hand) == 2:
+        print(f"the {player_hand[0][0]} of {player_hand[0][2]}"
+              f" and the {player_hand[1][0]} of {player_hand[1][2]}.")
+    else:
+        for card in player_hand[:-1]:
+            print(f"the {card[0]} of {card[2]}, ", end='')
+        print(f"and the {player_hand[-1][0]} of {player_hand[-1][2]}.")
+
+    print()
+    pause()
+
+def display_dealer_hand(dealer_hand):
+    prompt(f"{DEALER} has: an unknown card and the \
+{dealer_hand[0][0]} of {dealer_hand[0][2]}.")
+    print()
+    pause()
+
+def display_player_points(points):
+    prompt(f"Your current point total: {points}")
+    print()
+    pause()
+
+def is_bust(points):
+    return points > MAX_SCORE
+
+def ends_with_bust(winner):
+    if winner == DEALER:
+        prompt(f"Oh no! Your score went over {MAX_SCORE}. That's a bust!")
+        print()
+        pause()
+        prompt(f"{DEALER} wins this round.")
+    else:
+        prompt(f"{DEALER} busted! You win this round!")
+    print()
+    pause()
+    press_enter_to_continue_clear()
+
+def ends_with_stay(winner, player_points, dealer_points):
+    if winner == DEALER:
+        prompt(f"Sorry, {DEALER} wins this round.")
+
+    elif winner == 'tie':
+        prompt("It's a push! That means you tied, so no winners this round.")
+
+    else:
+        prompt("You win this round!")
+
+    pause()
+    print()
+    prompt(f'You had {player_points} and {DEALER} had {dealer_points}.')
+    pause()
+    print()
+    press_enter_to_continue_clear()
+    transition()
+
+def prompt_player_hit_or_stay(player_points):
+    prompt('Do you want to hit or stay?')
+    prompt("Enter 'h' for hit or 's' for stay: ", ending='')
+    response = input().casefold().strip()
+    while response not in ['h', 'hit', 's', 'stay']:
+        transition()
+        prompt('Sorry, it looks like you made a mistake in your input.')
+        print()
+        pause()
+        display_player_points(player_points)
+        prompt('Do you want to hit or stay?')
+        prompt("Enter 'h' for hit or 's' for stay: ", ending='')
+        response = input().casefold().strip()
+
+    return response
+
+def player_hits(deck, player_hand):
+    new_card = deal_card_from_deck(deck)
+    player_hand.append(new_card)
+    transition()
+    prompt("You chose to hit!")
+    print()
+    pause()
+    prompt(f"Your new card is the {new_card[0]} of {new_card[2]}.")
+    print()
+    press_enter_to_continue_clear()
+    pause()
+
+def dealer_thinking():
+    transition()
+    prompt(f"{DEALER} is thinking.", ending='')
+    pause()
+    flush_print()
+    print('.', end='')
+    pause()
+    flush_print()
+    print('.', end='')
+    pause()
+    flush_print()
+    print('.', end='')
+    pause()
+    flush_print()
+    print('.', end='')
+    pause()
+    flush_print()
+    transition()
+
+def dealer_will_stay(points):
+    return points >= DEALER_HIT_THRESHOLD
+
+def dealer_hits(deck, dealer_hand):
+    card = deal_card_from_deck(deck)
+    dealer_hand.append(card)
+
+def compare_hands_return_both_scores(player_hand, dealer_hand):
+    player_points = calculate_hand_total_points(player_hand)
+    dealer_points = calculate_hand_total_points(dealer_hand)
+
+    return player_points, dealer_points
+
+def return_winner_after_comparing(player_points, dealer_points, player_name):
+    if player_points > dealer_points:
+        return player_name
+    if player_points < dealer_points:
+        return DEALER
+    return 'tie'
+
+def display_round_results(winner, ending_action, player_points, dealer_points):
+    transition()
+
+    if ending_action == 'bust':
+        ends_with_bust(winner)
+        transition()
+        return
+
+    ends_with_stay(winner, player_points, dealer_points)
+
+def match_is_over(score_table):
+    return ROUNDS_TO_WIN in (score_table[0][1], score_table[1][1])
+
+def display_match_results(score_table):
+    press_enter_clear_this_line()
+
+    if score_table[0][1] == ROUNDS_TO_WIN:
+        prompt(F"Congratulations! That's {ROUNDS_TO_WIN}! You won the match!")
+    else:
+        prompt(F"Sorry, that's {ROUNDS_TO_WIN}. {DEALER} won the match.")
+
+    print()
+    pause()
+
+    press_enter_to_continue_clear()
+
+def prompt_play_next_round():
+    prompt("Would you like to continue to the next round?")
+    print()
+    prompt("Press 'y' or 'n': ", ending='')
+    response = return_valid_yn_response("Would you like to continue \
+to the next round?", input().casefold().strip())
+
+    return response in ['y', 'yes']
+
+def prompt_play_new_match():
+    prompt("Would you like to play a new match?")
+    print()
+    prompt("Press 'y' or 'n': ", ending='')
+    response = return_valid_yn_response("Would you like to play a new match?",
+                                        input().casefold().strip())
+
+    return response in ['y', 'yes']
+
+def play_twentyone():
+    player_name = get_name()
+    welcome_player(player_name)
+
+    score_table = init_score_table(player_name)
+
+    if prompt_player_wants_instructions():
+        instructions()
+
+    display_game_start(player_name)
+
+    while True:
+        deck = init_deck()
+        shuffle(deck)
+
+        player_hand, dealer_hand = deal_both_hands(deck)
+
+        one_round(deck, player_hand, dealer_hand, player_name, score_table)
+
+        display_scoreboard(score_table)
+
+        if match_is_over(score_table):
+            display_match_results(score_table)
+            if prompt_play_new_match():
+                play_twentyone()
+            else:
+                break
+
+        if not prompt_play_next_round():
+            break
+
+    transition()
+    prompt('Thank you for playing Twenty-One!')
+
+
+play_twentyone()
